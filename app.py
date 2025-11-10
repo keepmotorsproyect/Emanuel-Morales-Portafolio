f5efc46a8e52e8f03edad9a459a93f42ad8c6454
keepmotorsproyect <emanuel.moralesospina7@gmail.com>
2025-11-10 15:43:08 -0500

    1011

--- app.py
+++ app.py
@@ -7,12 +7,8 @@ def inicio():
     return'¡Hola mundo desde Flask!'
 
 @app.route('/saludo')
-def inicio():
+def saludo():
     return'Hola!'
-
-@app.route('/')
-def inicio():
-    return'Página de inicio'
 
 @app.route('/acerca')
 def acerca():
@@ -28,9 +24,7 @@ def mostrar_usuario(nombre):
 
 @app.route('/post/<int:post_id>')
 def mostrar_post(post_id):
-    return f'Mostrar el post número: {post_id}'
-
+    return f'Mostrar el post número:{post_id}'
 
 if __name__ == '__main__':
     app.run(debug=True)
-
